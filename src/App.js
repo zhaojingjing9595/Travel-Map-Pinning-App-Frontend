e23cfaec9ce64f90342a4c841c4165ff6acdcbcc
zhaojingjing95 <zhaojingjing9595@gmail.com>
2022-06-02 16:42:33 +0300

    add favicon, add newPin

--- src/App.js
+++ src/App.js
@@ -1,39 +1,73 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Map, { Marker, Popup } from "react-map-gl";
 import StarIcon from "@mui/icons-material/Star";
 import RoomIcon from "@mui/icons-material/Room";
 import "mapbox-gl/dist/mapbox-gl.css";
-import './app.css'
+import "./app.css";
 import axios from "axios";
-import {format} from 'timeago.js'
-
+import { format } from "timeago.js";
 
 function App() {
-   const currentUser = "Jingjing";
-   const [pins, setPins] = useState([]);
-   const [currentPlaceId, setCurrentPlaceId] = useState(null);
-   const [viewState, setViewState] = React.useState({
-     longitude: 35,
-     latitude: 31.6,
-     zoom: 6.5,
-   });
-  
+  const [currentUser, setCurrentUser] = useState(null);
+  const [pins, setPins] = useState([]);
+  const [currentPlaceId, setCurrentPlaceId] = useState(null);
+  const [newPlace, setNewPlace] = useState(null);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [rating, setRating] = useState(0);
+  const [viewState, setViewState] = React.useState({
+    longitude: 35,
+    latitude: 31.6,
+    zoom: 6.5,
+  });
+
   useEffect(() => {
-     async function getPins() {
-       try {
-         const response = await axios.get("http://localhost:8080/api/pins");
-         setPins(response.data);
-       } catch (error) {
-         console.log(error);
-       }
-     }
-     getPins();
-  }, [])
-  
-   function handleMarkerClick(id) {
-     setCurrentPlaceId(id);
-   }
-  
+    async function getPins() {
+      try {
+        const response = await axios.get("http://localhost:8080/api/pins");
+        setPins(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getPins();
+  }, []);
+
+  function handleMarkerClick(id, lat, long) {
+    setCurrentPlaceId(id);
+    setViewState({ ...viewState, latitude: lat, longitude: long });
+  }
+
+  function handleAddClick(e) {
+    setNewPlace({
+      lat: e.lngLat.lat,
+      long: e.lngLat.lng,
+    });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const newPin = {
+      username: currentUser,
+      title,
+      desc,
+      rating: parseInt(rating),
+      lat: newPlace.lat,
+      long: newPlace.long,
+    };
+    console.log(newPin);
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/pins",
+        newPin
+      );
+      setPins([...pins, response.data]);
+      setNewPlace(null);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <Map
       {...viewState}
@@ -42,19 +76,25 @@ function App() {
       //  mapStyle="mapbox://styles/mapbox/streets-v9"
       mapStyle="mapbox://styles/safak/cknndpyfq268f17p53nmpwira"
       mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+      onDblClick={handleAddClick}
+      transitionDuration="200"
     >
-
       {pins.length > 0 &&
         pins.map((p) => (
           <div key={p._id}>
-            <Marker longitude={p.long} latitude={p.lat}>
+            <Marker
+              longitude={p.long}
+              latitude={p.lat}
+              offsetLeft={-viewState.zoom * 3.5}
+              offsetRight={-viewState.z}
+            >
               <RoomIcon
                 style={{
                   fontSize: viewState.zoom * 7,
                   color: p.username === currentUser ? "tomato" : "slateblue",
                   cursor: "pointer",
                 }}
-                onClick={() => handleMarkerClick(p._id)}
+                onClick={() => handleMarkerClick(p._id, p.lat, p.long)}
               />
             </Marker>
             {p._id === currentPlaceId && (
@@ -75,11 +115,7 @@ function App() {
                   <p className="desc">{p.desc}</p>
                   <label>Rating</label>
                   <div className="stars">
-                    <StarIcon className="star" />
-                    <StarIcon className="star" />
-                    <StarIcon className="star" />
-                    <StarIcon className="star" />
-                    <StarIcon className="star" />
+                    {Array(p.rating).fill(<StarIcon className="star" />)}
                   </div>
                   <label>Information</label>
                   <span className="username">
@@ -91,6 +127,64 @@ function App() {
             )}
           </div>
         ))}
+      {newPlace && (
+        <>
+          <Marker longitude={newPlace.long} latitude={newPlace.lat}>
+            <RoomIcon
+              style={{
+                fontSize: viewState.zoom * 7,
+                color: "green",
+                cursor: "pointer",
+              }}
+              // onClick={() => handleMarkerClick(newPlace._id)}
+            />
+          </Marker>
+          <Popup
+            longitude={newPlace.long}
+            latitude={newPlace.lat}
+            anchor="right"
+            closeButton={true}
+            closeOnClick={false}
+            onClose={() => {
+              setNewPlace(null);
+            }}
+          >
+            <div>
+              <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input
+                  placeholder="Enter a title"
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Review</label>
+                <textarea
+                  placeholder="say something about this place"
+                  onChange={(e) => setDesc(e.target.value)}
+                />
+                <label>Rating</label>
+                <select onChange={(e) => setRating(e.target.value)}>
+                  <option value={1}>1</option>
+                  <option value={2}>2</option>
+                  <option value={3}>3</option>
+                  <option value={4}>4</option>
+                  <option value={5}>5</option>
+                </select>
+                <button className="submitButton" type="submit">
+                  Add Pin
+                </button>
+              </form>
+            </div>
+          </Popup>
+        </>
+      )}
+      {currentUser ? (
+        <button className="button logout">Logout</button>
+      ) : (
+        <div className="buttons">
+          <button className="button login">Login</button>
+          <button className="button register">Register</button>
+        </div>
+      )}
     </Map>
   );
 }
